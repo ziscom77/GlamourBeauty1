18e5cafb65bf876745b45cc447874edbf211aac0
ziscom77 <etorrbon@myuax.com>
2025-06-06 02:34:59 +0200

    GlamourFinancial Arreglado

--- src/GlamourFinancial.java
+++ src/GlamourFinancial.java
@@ -1,10 +1,7 @@
+/* GlamourFinancial.java */
 public interface GlamourFinancial {
-
-    /* CRUD de la cotización en memoria o BD */
     double obtenerCotizacion(String mercado);
     void   actualizarCotizacion(String mercado, double valor);
-
-    /* Indicadores */
-    double calcularValorDeMercado();      // precio medio × acciones en circulación
-    double calcularPrecioValorEnLibros(); // valor de mercado / valor en libros
+    double calcularValorDeMercado();
+    double calcularPrecioValorEnLibros();
 }
